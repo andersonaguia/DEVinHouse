1f738b2fe210f9748638635217d012b875d0911d
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-07-06 22:57:42 -0300

    first commit

---
+++ semana2/index.js
@@ -0,0 +1,8 @@
+var convidado = 'Anderson'
+var data = '22/04/2022'
+var hora = '22'
+
+console.log(`
+Caro ${convidado}, convidamos voce ao nosso 
+casamento que acontecera dia ${data} as ${hora}h. Tudo certo!
+`)
