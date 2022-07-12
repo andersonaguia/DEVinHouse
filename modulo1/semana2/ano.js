8603cf96f510501c890e4f552393b6b78d6a287a
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-07-12 00:15:55 -0300

    [M1S02] Ex 9 - Crie um contador

--- modulo1/semana2/ano.js
+++ modulo1/semana2/ano.js
@@ -1,6 +1,25 @@
-const ano = prompt("Digite um ano:");
+//Calculo de diferença do ano digitado
+/*const ano = prompt("Digite um ano:");
 const atual = new Date().getFullYear();
 
 diferenca = atual - parseInt(ano);
 
 alert("Diferença: " + diferenca + " anos.")
+*/
+//Incremento e decremento de valores
+let resultado = 0;
+let valor = document.getElementById("valor");
+
+let incremento = document.getElementById("incremento");
+let decremento = document.getElementById("decremento");
+
+incremento.addEventListener("click", function(){
+    resultado += parseInt(incremento.value);
+    valor.innerText = resultado;
+});
+
+decremento.addEventListener("click", function(){
+    resultado += parseInt(decremento.value);
+    valor.innerText = resultado;
+});
+
