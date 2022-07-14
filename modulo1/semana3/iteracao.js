7aef9f6ef9a32b23640c33df05b7a217785c4382
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-07-14 08:02:24 -0300

    [M1S03] Ex 2 - Iterando sobre arrays de objetos #4

---
+++ modulo1/semana3/iteracao.js
@@ -0,0 +1,23 @@
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+    ];
+    
+    listaConvidados.forEach(function(valor, i){  
+        item = document.createElement("li"); //Cria um elemento <li> no documento HTML
+        nome = listaConvidados[i].nome + " " + listaConvidados[i].sobrenome; //Guarda os valores contidos no índice [i] do array, dentro das chaves '.nome' e '.sobrenome' do objeto contido nesse índice dentro de uma variável chamada 'nome'
+        item.textContent = nome; //Adiciona ao item o conteúdo contido na variável 'nome'
+        lista1.appendChild(item); //Adiciona o conteúdo contido na variável 'item' ao documento HTML                
+    });
+
+    
+
+   
--- modulo1/semana3/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-let listaNumeros = [ 2, 3, 9, 12, 16, 55, 90, 78, 17 ];
-
-listaNumeros.forEach((lista) => {
-    console.log(lista*2);   
-});
