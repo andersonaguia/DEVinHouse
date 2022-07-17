63ac8ed57e5a339d18114a77889e3c16532c9e4b
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-07-16 21:38:20 -0300

    criando objeto #9

---
+++ modulo1/semana3/ex4-objeto/index.js
@@ -0,0 +1,36 @@
+let objeto = [
+    {nome: "Anderson",
+    sobrenome: "Aguiar",
+    setor: "camarote",
+    idade: 34
+    },
+    {nome: "José",
+    sobrenome: "Augusto",
+    setor: "pista",
+    idade: 29
+    },
+    {nome: "Patrícia",
+    sobrenome: "Rocha",
+    setor: "camarote",
+    idade: 34
+    },
+    {nome: "Gilberto",
+    sobrenome: "Junior",
+    setor: "arquibancada",
+    idade: 17
+    },
+    {nome: "André",
+    sobrenome: "Mendes",
+    setor: "arquibancada",
+    idade: 18
+    },
+    {nome: "Andreza",
+    sobrenome: "Sanny",
+    setor: "pista",
+    idade: 17
+    }
+]
+
+let listaDeConvidados = [objeto];
+
+console.log(listaDeConvidados);
