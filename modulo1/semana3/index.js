37625e8c581ef320aea2dca9db529fa952874649
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-07-16 21:59:10 -0300

    criando objetos #10

---
+++ modulo1/semana3/index.js
@@ -0,0 +1,37 @@
+let objeto1 = {
+    nome: "Anderson",
+    sobrenome: "Aguiar",
+    setor: "camarote",
+    idade: 34
+}
+let objeto2 = {
+    nome: "André",
+    sobrenome: "Mendes",
+    setor: "arquibancada",
+    idade: 18
+}
+let objeto3 = {
+    nome: "Gilberto",
+    sobrenome: "Junior",
+    setor: "arquibancada",
+    idade: 17
+}
+let objeto4 = {
+    nome: "Andreza",
+    sobrenome: "Sanny",
+    setor: "pista",
+    idade: 17
+}
+let objeto5 = {    
+    nome: "Patrícia",
+    sobrenome: "Rocha",
+    setor: "camarote",
+    idade: 34
+}
+let objeto6 = {
+    nome: "José",
+    sobrenome: "Augusto",
+    setor: "pista",
+    idade: 29    
+}    
+
